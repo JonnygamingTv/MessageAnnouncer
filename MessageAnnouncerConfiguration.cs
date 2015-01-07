4a6347f6bc215fc95f750cdb29c2f28ca930ecb4
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-01-07 13:02:26 +0100

    Update config to 2.0.2.0

--- MessageAnnouncerConfiguration.cs
+++ MessageAnnouncerConfiguration.cs
@@ -1,18 +1,29 @@
 ﻿
+using Rocket.RocketAPI;
 namespace unturned.ROCKS.MessageAnnouncer
 {
-    public class MessageAnnouncerConfiguration
+    public class MessageAnnouncerConfiguration : RocketConfiguration
     {
-        public int Interval = 180;
-        public string[] Messages = { 
-            "Welcome to unturned.ROCKS, we hope you enjoy your stay!",
-            "Join our TeamSpeak 3 server at unturned.ROCKS!",
-            "Please chat in english. Be polite.",
-            "We are searchin staff, Apply on our forum!",
-            "Check out our forum at forum.unturned.ROCKS",
-            "If you have any questions ask an admin on our TeamSpeak 3 server!",
-            "Please chat in english. Be polite.",
-            "We are searchin staff, Apply on our forum!",
-        };
+        public int Interval;
+        public string[] Messages;
+
+        public RocketConfiguration DefaultConfiguration
+        {
+            get {
+                MessageAnnouncerConfiguration config = new MessageAnnouncerConfiguration();
+                config.Messages = new string[]{ 
+                    "Welcome to unturned.ROCKS, we hope you enjoy your stay!",
+                    "Join our TeamSpeak 3 server at unturned.ROCKS!",
+                    "Please chat in english. Be polite.",
+                    "We are searchin staff, Apply on our forum!",
+                    "Check out our forum at forum.unturned.ROCKS",
+                    "If you have any questions ask an admin on our TeamSpeak 3 server!",
+                    "Please chat in english. Be polite.",
+                    "We are searchin staff, Apply on our forum!",
+                };
+                config.Interval =  180;
+                return config;
+            }
+        }
     }
 }
