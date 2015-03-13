a36920d13c6ca825116e71b6fe1cd9fa7d193a0f
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-03-14 00:25:46 +0100

    Updated to Rocket 3.0

--- MessageAnnouncerConfiguration.cs
+++ MessageAnnouncerConfiguration.cs
@@ -16,7 +16,7 @@ namespace unturned.ROCKS.MessageAnnouncer
                     "Join our TeamSpeak 3 server at unturned.ROCKS!",
                     "Please chat in english. Be polite.",
                     "We are searchin staff, Apply on our forum!",
-                    "Check out our forum at forum.unturned.ROCKS",
+                    "Check out our forum at https://unturned.ROCKS",
                     "If you have any questions ask an admin on our TeamSpeak 3 server!",
                     "Please chat in english. Be polite.",
                     "We are searchin staff, Apply on our forum!",
